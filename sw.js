fc76da5f79648d31c83a6620d2fd2c0278cd8e58
Yurii Tatar <y.tatar@andersenlab.com>
2022-07-28 19:32:42 +0300

    Playing with SW

--- sw.js
+++ sw.js
@@ -1,6 +1,25 @@
 const VERSION = 'v3';
 
 self.addEventListener('install', event => event.waitUntil(installServiceWorker()));
+self.addEventListener('fetch', event => event.respondWith(cacheThenNetwork(event)));
+
+async function cacheThenNetwork(event) {
+
+    const cache = await caches.open(VERSION);
+
+    const cachedResponse = await cache.match(event.request);
+
+    if (cachedResponse) {
+        console.log('Serving From Cache: ' + event.request.url);
+        return cachedResponse;
+    }
+
+    const networkResponse = await fetch(event.request);
+
+    console.log('Calling network: ' + event.request.url);
+
+    return networkResponse;
+}
 
 
 async function installServiceWorker() {
