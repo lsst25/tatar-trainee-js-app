2c628eb57abf072a018654ed24cc468f49157114
Yurii Tatar <y.tatar@andersenlab.com>
2022-07-28 19:14:43 +0300

    Playing with SW

--- sw.js
+++ sw.js
@@ -1,29 +1,18 @@
-const VERSION = 'v2.2';
+const VERSION = 'v3';
 
-self.addEventListener('install', event => {
-    log("INSTALLING ");
-    const installCompleted = Promise.resolve()
-        .then(() => log("INSTALLED"));
-
-    event.waitUntil(installCompleted);
-});
-
-self.addEventListener('activate', event => {
-    log("ACTIVATING");
-    const activationCompleted = Promise.resolve()
-        .then((activationCompleted) => log("ACTIVATED"));
-
-    event.waitUntil(activationCompleted);
-});
-
-// handling service worker installation
-self.addEventListener('fetch', event => {
-    log("HTTP call intercepted - " + event.request.url);
-    return event.respondWith(fetch(event.request.url));
-});
+self.addEventListener('install', event => event.waitUntil(installServiceWorker()));
 
 
-// each logging line will be prepended with the service worker version
-function log(message) {
-    console.log(VERSION, message);
+async function installServiceWorker() {
+
+    log("Service Worker installation started ");
+
+    const cache = await caches.open(VERSION);
+
+    return cache.addAll([
+        '/',
+        'app.js',
+        'style.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+    ]);
 }
