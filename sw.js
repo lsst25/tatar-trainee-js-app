da8669c932dfac43f576f61973106596f5381288
Yurii Tatar <y.tatar@andersenlab.com>
2022-07-28 19:24:55 +0300

    Playing with SW

--- sw.js
+++ sw.js
@@ -14,5 +14,6 @@ async function installServiceWorker() {
         'app.js',
         'style.css',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+        'https://api.punkapi.com/v2/beers',
     ]);
 }
