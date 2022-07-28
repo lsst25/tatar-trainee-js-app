c1710d43c7f85bb232e39482c5d345aa87e952b8
Yurii Tatar <y.tatar@andersenlab.com>
2022-07-28 18:58:01 +0300

    SW testing

--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load',  () => {
         navigator.serviceWorker.register('./sw.js', {
-            scope: '/'
+            scope: '/tatar-trainee-js-app/'
         })
             .then(registration => {
                 console.log("Service Worker registration completed ...");
